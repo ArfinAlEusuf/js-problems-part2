c48713c3ffaf605d1fed42eb4e070c2952481bf2
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-06 00:01:32 +0600

    get ultimate max

--- max.js
+++ max.js
@@ -18,5 +18,7 @@ function getMax(num1, num2){
     }
 }
 
-const max = getMax(56,79);
-console.log('max of two is: ', max)
+const max1 = getMax(56,79);
+const max2 = getMax(56,98);
+const ultimateMax = getMax(max1,max2)
+console.log('max of two is: ', ultimateMax)
