312aef8ff42bc068de848c6372f506dd55135a67
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-08-05 23:59:22 +0600

    get the max number

--- max.js
+++ max.js
@@ -1,1 +1,22 @@
 const disha = 56;
+const salman = 95;
+
+if (disha>salman){
+    console.log('Disha will get the strobery')
+}
+else{
+    console.log('salman will eat the stroberry')
+}
+
+// inside a function
+function getMax(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
+const max = getMax(56,79);
+console.log('max of two is: ', max)
